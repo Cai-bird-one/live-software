15b832d41864fb5e8846634e4e08dfc501647f39
Cai-bird-one <birdcly@Birdcly>
2025-07-13 23:11:23 +0800

    add model call

---
+++ src/core/node.py
@@ -0,0 +1,34 @@
+
+
+class node:
+    """
+    A node in the DAG
+    """
+    
+    def __init__(self, path):
+        self.path = path
+    
+    def get_codes():
+        """
+        Returns the code of the node
+        """
+        pass
+    
+    def change_codes(self, codes:str):
+        """
+        Changes the code of the node
+        """
+        pass
+    
+    
+    def add_method(self, mothod:str):
+        """
+        Adds a method to the node
+        """
+        pass
+
+    def get_sons(self):
+        """
+        Returns the sons of the node
+        """
+        pass
