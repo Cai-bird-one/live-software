a614a2024b5f9bd0364c94d9f5777afb9ed178bc
Cai-bird-one <birdcly@Birdcly>
2025-07-14 19:04:28 +0800

    add an simple example

--- src/core/agent.py
+++ src/core/agent.py
@@ -1,7 +1,9 @@
 from ..utils.config import Config
 from ..llm.llm_client import LLMClient
 from ..llm.llm_basics import LLMMessage, LLMResponse
-
+from ..utils.templates import modify_code_template
+from ..utils.running import run_local_code_in_docker
+import json
 config = Config()
 
 
@@ -9,7 +11,38 @@ class LiveSoftware:
     design: str
     code: str
     def __init__(self, config):
-        self.client = LLMClient(config)
+        self.client = LLMClient(config.default_provider, config.model_providers[config.default_provider])
+        self.design = ""
         self.code = ""
-    def add_method(self, method: str):
-        pass
+        # clear /home/birdcly/live software/codes/main.py
+        with open("/home/birdcly/live software/codes/main.py", "w") as f:
+            f.write("")
+    def get_code(self):
+        with open("/home/birdcly/live software/codes/main.py", "r") as f:
+            self.code = f.read()
+        return self.code
+    def set_code(self, code: str):
+        self.code = code
+        with open("/home/birdcly/live software/codes/main.py", "w") as f:
+            f.write(code)
+    def get_design(self):
+        return self.design
+    def set_design(self, design: str):
+        self.design = design
+    def modify_code(self, message: str):
+        response = self.client.chat([LLMMessage(role="user", content=modify_code_template(self.design, self.code, message))],model_parameters=config.model_providers[config.default_provider])
+        # print(response)
+        response = json.loads(response.content)
+        self.set_design(response["design"])
+        self.set_code(response["code"])
+        return response["code"]
+
+
+if __name__ == "__main__":
+    live_software = LiveSoftware(config)
+    live_software.modify_code("you need to create a calculator that can add one and two")
+    print("results= ", run_local_code_in_docker("/home/birdcly/live software/codes", "main.py"))
+
+
+    print("done")
+
--- src/utils/templates.py
+++ src/utils/templates.py
@@ -1,12 +1,35 @@
 import json
-def get_add_method_prompt(codes, request):
-    f"""
+# def get_add_method_prompt(codes, request):
+#     f"""
+#     You are an expert AI software engineering agent.
+#     Your task is to maintain a Python code class. Below is the code for this class along with corresponding explanations, as well as the code for its members and subclasses:
+#     {json.dump(codes, indent=4, ensure_ascii=False)}
+#     The task you need to complete is:
+#     {request}
+#     You need to return the whole code for the class that support the request.
+#     You can also ask your members or subclasses to add some method, or add a new subclasses or a new class.
+#     The returned content must be in JSON format, with the outermost layer including "code", "add method", and "add class". The keys within "add method" and "add class" should be the class names, and the values should be the requirements
+#     """
+
+# def add_subclass_method(codes, request):
+#     f"""
+#     You are an expert AI software engineering agent.
+#     Your task is to maintain a Python code class. Below is the code for this class along with corresponding explanations, as well as the code for its members and subclasses:
+#     {json.dump(codes, indent=4, ensure_ascii=False)}
+#     Now you need to create a new subclass of the class and maintain the code, this subclass should follow::
+#     {request}
+#     You can also ask your members or subclasses to add some method, or add a new subclasses or a new class.
+#     The returned content must be in JSON format, with the outermost layer including "code", "add method", and "add class". The keys within "add method", "add subclass" "add class"should be the class names, and the values should be the requirements
+#     """
+def modify_code_template(designs, codes, request):
+    return f"""
     You are an expert AI software engineering agent.
-    Your task is to maintain a Python code class. Below is the code for this class along with corresponding explanations, as well as the code for its members and subclasses:
-    {json.dump(codes, indent=4, ensure_ascii=False)}
-    The task you need to complete is:
-    {request}:
-    You need to return the whole code for the class that support the request.
-    You can also ask your members or subclasses to add some method, or add a new subclasses or a new class.
-    The returned content must be in JSON format, with the outermost layer including "code", "add method", and "add class". The keys within "add method" and "add class" should be the class names, and the values should be the requirements
+    Your task is to maintain a python code, and solve some problems.
+    There has been several requests and this is the design for the code:
+    {json.dumps(designs, indent=4, ensure_ascii=False)}
+    The code is as follows:
+    {json.dumps(codes, indent=4, ensure_ascii=False)}
+    You need to complete the design and code to solve the problems:
+    {request}
+    The returned content must be in JSON format, with the key "design" and "code", and the values should be the corresponding design and code.
     """
